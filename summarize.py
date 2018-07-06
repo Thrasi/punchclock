e68e3ed9c814403defd1cbeeb29d1e7175d9c97d
magnus-benediktsson <magnus.benediktsson@aphelion.se>
2018-07-06 21:36:01 +0200

    Added proper argument parsing that allows for summarizing specific cases and/or certain files

--- summarize.py
+++ summarize.py
@@ -4,22 +4,24 @@ from datetime import datetime
 from collections import defaultdict
 import sys
 import os
-"""
-python summarize.py 				: summarizes all days
-python summarize.py [file]			: summarizes days from file
-python summarize.py [file] [case]	: summarizes case from the file
-"""
+import argparse
+
+parser = argparse.ArgumentParser(description='Summarize time logs.')
+parser.add_argument('-c', '--case', help="Case number to summarize e.g. ABC-1234")
+parser.add_argument('-f', '--files', nargs='+', help="Files to summarize")
+args = parser.parse_args()
+
 dates = defaultdict( lambda: defaultdict( float ) )
 date_format='%Y-%m-%d %H-%M-%S'
 target_case=""
-FILES=[]         
+FILES=os.listdir("logs")
 case_total = 0
-if len(sys.argv) > 1:
-	FILES.append(sys.argv[1])
-	if len(sys.argv) > 2:
- 		target_case = sys.argv[2]
-else:
-	FILES=os.listdir("logs")
+
+if args.files != None:
+	FILES = args.files
+
+if args.case != None:
+	target_case = args.case
 
 for file_path in FILES:
 	with open( os.path.join("logs",file_path) ) as f:
@@ -44,12 +46,13 @@ for date in sorted( dates.keys() ):
                 print
         else:
             if target_case in dates[date]:
+            	print "-----------------------------------------"
                 print "{}:".format(date)
                 print "\t{:<15}{:04.2f}".format( target_case, dates[date][target_case] )
                 case_total += dates[date][target_case]
 if target_case!="":
         print
         print "Total hours on {:<12}{:04.2f}".format( str(target_case)+":", case_total )
-        print
+        print "-----------------------------------------"
 
 
